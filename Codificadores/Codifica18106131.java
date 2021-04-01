cdd6d677682d122634e07e1ae9d1513b03b926f9
Gabriel Tasca Villa <gabriel.villa@edu.pucrs.br>
2021-04-01 00:06:31 -0300

    Adicionando elementos na tabela

--- Codificadores/Codifica18106131.java
+++ Codificadores/Codifica18106131.java
@@ -1,8 +1,8 @@
 package Codificadores;
 
 public class Codifica18106131 implements Codifica {
-    public String alfabetoNormal = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-    public String alfabetoAlterado = "bcaefdhigkjlnomqrptuswxvyzBCAEFDHIGKLJNOMQRPTUSWXVZY*";
+    public String alfabetoNormal = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ:çãâôõÇÃÂÔÕÉ/*?{}()[]";
+    public String alfabetoAlterado = "bcaefdhigkjlnomqrptuswxvyzBCAEFDHIGKLJNOMQRPTUSWXVZY-ôçõâãÔÇÕÂÉÃ()[]*?/{}";
 
     
     public String codifica(String str){
